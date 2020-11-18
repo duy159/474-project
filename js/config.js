e2c09611c49be2ac8b1b363476cdcbdf43f67e40
owaisrana <74514225+owaisrana@users.noreply.github.com>
2020-11-17 17:34:55 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8FpjfynvS', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6d7lnfsct563de78a9k6fq584p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_LCwhfogEK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4l86v8qdahb1kfib8gmbqhgqa6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1 ' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://nuiycewn3j.execute-api.us-east-1.amazonaws.com/prod',
+        invokeUrl: 'https://os0acah3e8.execute-api.us-east-1.amazonaws.com/prod',
     }
 };
